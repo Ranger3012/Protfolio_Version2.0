53dd192c99aa64ebdea2047478cd649af2c72b18
Ranger3012 <78731159+Ranger3012@users.noreply.github.com>
2024-10-25 12:23:33 -0400

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -19,3 +19,14 @@ const nav = document.querySelector("nav");
 burger.addEventListener("click", () => {
     ul.classList.toggle("show");
 });
+
+// Close burger menu when a link is clicked
+
+// Select nav links
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach((link) => 
+link.addEventListener("click", () => {
+    ul.classList.remove("show");
+})
+);
